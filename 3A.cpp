5eabc9a7fbfed4ecc8757c30603cf9550c462edb
lokendrasingh6 <94732054+lokendrasingh6@users.noreply.github.com>
2023-11-08 23:27:14 +0530

    Shortest Job first (Non-preemptive)

--- 3A.cpp
+++ 3A.cpp
@@ -3,71 +3,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"Enter the total number of processes"<<endl;
+    // cout<<"Enter the total number of processes"<<endl;
     int p;
     cin>>p;
     int arrival_time[1000];
     int burst_time[1000];
     vector<pair<int,int>>pr;
-    cout<<"Enter the Details of each process"<<endl;
+    // cout<<"Enter the Details of each process"<<endl;
+    int sum=0;
     for(int i=0;i<p;i++){
-        cout<<"Enter the arrival time of the process "<<i+1<<endl;
-        cin>>arrival_time[i+1];
-        cout<<"Enter the burst time of the process "<<i+1<<endl;
-        cin>>burst_time[i+1];
+        // cout<<"Enter the arrival time of the process "<<i+1<<endl;
+        cin>>arrival_time[i];
+        // cout<<"Enter the burst time of the process "<<i+1<<endl;
+        cin>>burst_time[i];
+        sum=sum+burst_time[i];
     }
-    vector<int>v;
     vector<int>gaant;
+    map<int,int>m;
+    int time=0;
     int cnt=0;
+    int r=0;
+    int min=-1;
+    int fuck=0;
+    int l=-1;
     while(1){
         for(int i=0;i<p;i++){
-            if(arrival_time[i+1]==cnt){
-                v.push_back(i+1);
+            if(time==arrival_time[i]){
+                m[i]++;
             }
-        }
-        int min=INT_MAX;
-        int l=-1;
-        for(int i=0;i<v.size();i++){
-            if(burst_time[v[i]]<min and burst_time[v[i]]!=0){
-                min=burst_time[v[i]];
-                l=v[i];
             }
-        }
-        if(min==INT_MAX){
+            if(cnt==0){
+                min=INT_MAX;
+                 l=-1;
+                for(int j=0;j<p;j++){
+                    if(burst_time[j]<min and m[j]==1){
+                        min=burst_time[j];
+                        l=j;
+                    }
+                }
+                fuck++;
+                m[l]++;
+                cnt=1;
+            }
+            min--;
+            if(min==0){
+                cnt=0;
+            }
+            gaant.push_back(l+1);
+
+        time++;
+        if(sum==time){
             break;
         }
-        if(l!=-1){
-            burst_time[l]--;
         }
-        
-        gaant.push_back(l);
-        cnt++;
-    }
-    
-    cout<<endl;
-    cout<<"The Gaant Chart"<<endl;
-    for(int i=0;i<gaant.size();i++){
-        cout<<gaant[i]<<" ";
-    }
-    cout<<endl;
-    cout<<gaant.size()<<endl;
 
-    cout<<"completion time"<<endl;
-    vector<int>completion;
-    for(int i=0;i<p;i++){
-        int xoor=-1;
-        for(int j=gaant.size()-1;j>=0;j--){
-            if(gaant[j]==i+1){
-                xoor=j;
-                break;
-            }
+        for(int i=0;i<gaant.size();i++){
+            cout<<gaant[i]<<" ";
         }
-        cout<<"process "<<i+1<<" completion time: "<<xoor+1<<endl;
-        completion.push_back(xoor+1);
+        cout<<endl;
     }
-    //similarly u can find turn around time and waiting time
 
 
 
-}
 
